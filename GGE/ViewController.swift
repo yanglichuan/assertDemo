4c9405ab50973ca271367ed9b550788225b5b88d
yanglichuan <yanglichuancom@163.com>
2015-12-04 14:32:27 +0800

    断言

---
+++ GGE/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  GGE
+//
+//  Created by  yanglc on 15/12/4.
+//  Copyright © 2015年  yanglc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var a = 1;
+        var b = 2;
+        
+        assert(a > b, "wfsdfsdfs")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
